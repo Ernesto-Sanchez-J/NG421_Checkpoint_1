4b0c9a15f959128ff8b07c418b280092a9e67a86
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-06-21 21:15:15 -0500

    added a todo edit component

--- src/app/TodoTable/todo-table/todo-table.component.ts
+++ src/app/TodoTable/todo-table/todo-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {ITodo} from '../interfaces/itodo';
+import { ITodo } from '../interfaces/itodo';
 import { TodoService } from '../services/todo.service';
 import { MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
