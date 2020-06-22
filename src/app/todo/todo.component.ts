c69976d313ba1e2d65883ee16e7b4993d683843f
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-06-21 20:00:10 -0500

    Finished ToDo component

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,33 +1,52 @@
-import { Component, OnInit,Input } from '@angular/core';
-import {TodoService} from '../services/todo.service';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Input } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationModalComponent } from '../confirmation-modal/confirmation-modal.component';
+import { ITodo } from '../interfaces/itodo';
+import { TodoEditComponent } from '../todo-edit/todo-edit.component';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-  @Input() todo
-  constructor(private todoService : TodoService, private modalService : NgbModal) { }
-  todoTitle = ''
-  ngOnInit() {
-  }
-  async deleteTodo(todo){
+  @Input() todo: ITodo;
+
+  constructor(
+    private todoService: TodoService,
+    private modalService: NgbModal
+  ) {}
+
+  todoTitle = '';
+  isEditing = false;
+  ngOnInit() {}
+
+  async deleteTodo(todo) {
     let result;
     const modal = this.modalService.open(ConfirmationModalComponent);
     modal.componentInstance.modalInstance = modal;
     try {
       result = await modal.result;
-      if(result === "yes") {
+      if (result === 'yes') {
         this.todoService.deleteTodo(todo);
       }
-    }
-    catch(ex){
-      
-    }
+    } catch (ex) {}
   }
 
+  getStatuses() {
+    return this.todoService.getStatuses();
+  }
 
+  async TodoEdit(todo: ITodo) {
+    let result;
+    const modal = this.modalService.open(TodoEditComponent);
+    modal.componentInstance.modalInstance = modal;
+    modal.componentInstance.todo = todo;
+
+    try {
+      result = await modal.result;
+      this.todo.description = result;
+    } catch (ex) {}
+  }
 }
